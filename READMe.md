841862c681eb7021283fd28b026cf78d51ba4df4
Guenther <gzoppelt1@gmail.com>
2016-08-25 07:20:59 +0100

    material reinstalled

--- READMe.md
+++ READMe.md
@@ -1,7 +1,9 @@
 #ac5
 AngularClass webpack-starter, based on rc.5
+
 * [downloaded from GitHub](https://github.com/AngularClass/angular2-webpack-starter.git)
 * [original README](doc/README.md)
-* suggested material2 branch was still with rc.4
+* suggested material2 branch was not up to date
 * so this one starts out with regular starter
+* remove /doc folder from .gitignore  
 * [and material 2 is added](doc/addMaterial.md)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,27 @@ import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 import { removeNgStyles, createNewHosts } from '@angularclass/hmr';
 
+import { MdCoreModule } from '@angular2-material/core';
+import { MdButtonModule } from '@angular2-material/button';
+import { MdButtonToggleModule } from '@angular2-material/button-toggle';
+import { MdCardModule } from '@angular2-material/card';
+import { MdCheckboxModule } from '@angular2-material/checkbox';
+import { MdGridListModule } from '@angular2-material/grid-list';
+import { MdIconModule } from '@angular2-material/icon';
+import { MdInputModule } from '@angular2-material/input';
+import { MdListModule } from '@angular2-material/list';
+import { MdMenuModule } from '@angular2-material/menu';
+import { MdProgressBarModule } from '@angular2-material/progress-bar';
+import { MdProgressCircleModule } from '@angular2-material/progress-circle';
+import { MdRadioModule } from '@angular2-material/radio';
+import { MdSidenavModule } from '@angular2-material/sidenav';
+import { MdSliderModule } from '@angular2-material/slider';
+import { MdSlideToggleModule } from '@angular2-material/slide-toggle';
+import { MdTabsModule } from '@angular2-material/tabs';
+import { MdToolbarModule } from '@angular2-material/toolbar';
+import { MdTooltipModule } from '@angular2-material/tooltip';
+
+
 /*
  * Platform and Environment providers/directives/pipes
  */
@@ -39,7 +60,26 @@ const APP_PROVIDERS = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(ROUTES, { useHash: true })
+    RouterModule.forRoot(ROUTES, { useHash: true }),
+    MdCoreModule,
+    MdButtonModule,
+    MdButtonToggleModule,
+    MdCardModule,
+    MdCheckboxModule,
+    MdGridListModule,
+    MdIconModule,
+    MdInputModule,
+    MdListModule,
+    MdMenuModule,
+    MdProgressBarModule,
+    MdProgressCircleModule,
+    MdRadioModule,
+    MdSidenavModule,
+    MdSliderModule,
+    MdSlideToggleModule,
+    MdTabsModule,
+    MdToolbarModule,
+    MdTooltipModule
   ],
   providers: [ // expose our Services and Providers into Angular's dependency injection
     ENV_PROVIDERS,
